f643252f848b45b87d89ae343d3411915c81b276
Moataz Nabil <45570191+moatazeldebsy@users.noreply.github.com>
2025-01-02 11:32:33 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/test-automation-practices/', // Add repository name back to base URL
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  base: '/',
+  build: {
+    // Ensure _redirects file is copied to the build output
+    copyPublicDir: true
+  }
 });
