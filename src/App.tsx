7fd3b2dc6146eb495db36a69be770894c1477db3
Moataz Nabil <45570191+moatazeldebsy@users.noreply.github.com>
2025-02-26 09:17:00 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { ThemeToggle } from './components/ThemeToggle';
 import { SideNav } from './components/SideNav';
